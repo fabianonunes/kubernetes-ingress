4519b5eb65a29d33f901d3f30b72e058ef212ae2
Fabiano Parente <fabiano@nunes.me>
2021-05-25 22:26:38 -0300

    BUG/MINOR: use IPs passed via ipv4-bind-address and ipv4-bind-address on TCP services

--- controller/handler.go
+++ controller/handler.go
@@ -45,6 +45,10 @@ func (c *HAProxyController) initHandlers() {
 		handler.TCPServices{
 			SetDefaultService: c.setDefaultService,
 			CertDir:           c.Cfg.Env.FrontendCertDir,
+			IPv4:              !c.OSArgs.DisableIPV4,
+			IPv6:              !c.OSArgs.DisableIPV6,
+			AddrIPv4:          c.OSArgs.IPV4BindAddr,
+			AddrIPv6:          c.OSArgs.IPV6BindAddr,
 		},
 		handler.PatternFiles{},
 		handler.Refresh{},
